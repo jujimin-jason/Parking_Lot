c16b1f83806f605211606ea3f091e893def1fc7f
suhyi <suhyi@192.168.0.19>
2025-07-10 14:47:45 +0900

    21

--- Parking_Lot/src/main/java/com/boot/parking/model/Member.java
+++ Parking_Lot/src/main/java/com/boot/parking/model/Member.java
@@ -8,5 +8,5 @@ public class Member {
 	private int mid; // 사용자 db id
 	private String mem_id; // 사용자 아이디
 	private String mem_pwd; // 사용자 비밀번호
-	private int store_code;
+	private int store_code; // 매장 코드 (0 = 관리자, 1 이상 = 매장 번호)
 }
